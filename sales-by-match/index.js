f8722f40c754b220a9f4d75bddf099a302ff9c03
Elen Salustiano <elen.salustiano@dextra-sw.com.br>
2020-10-17 22:11:08 -0300

    feat: add sales by match problem

---
+++ sales-by-match/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString.replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+  let total = []
+  ar.sort()
+  for (let i = 0; i < n; i++) {
+    const match = ar.indexOf(ar[i], i + 1)
+    if (match != -1 && !total.includes(i) && !total.includes(match)) {
+      total.push(i)
+      total.push(match)
+    }
+  }
+  return total.length / 2
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine(), 10);
+
+  const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+  let result = sockMerchant(n, ar);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
